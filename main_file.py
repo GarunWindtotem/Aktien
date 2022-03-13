84bbf545bb49c8a95e806b96bc192d3aacb14642
GarunWindtotem <63738678+GarunWindtotem@users.noreply.github.com>
2022-03-13 14:12:56 +0100

    u

---
+++ main_file.py
@@ -0,0 +1,8 @@
+# from rechenarten import *
+
+from rechenarten import addieren as ad
+
+a = 2
+b = 1
+
+print(ad(a,b))
---
+++ rechenarten.py
@@ -0,0 +1,13 @@
+def addieren(a,b):
+    c = a + b
+    return c
+
+def subtrahieren(a,b):
+    c = a - b
+    return c
+
+# z=addieren(3,5)
+# print(z)
+
+# z=subtrahieren(3,5)
+# print(z)
