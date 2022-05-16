a64df58f564ac58843de808a2223672acbb4f002
GarunWindtotem <63738678+GarunWindtotem@users.noreply.github.com>
2022-05-16 19:36:30 +0200

    u

--- daten.py
+++ daten.py
@@ -17,14 +17,14 @@ def daten():
     # Anzahl Aktien
     dict_n_aktien = {
         'LHA.DE': 148,
-        'H4ZJ.DE': 137.195,     # HSBC MSCI World
+        'H4ZJ.DE': 140.949,     # HSBC MSCI World
         # 'E127.MI': 6.258,
         'BTC-EUR': 0}
 
     # Einstandspreis
     dict_EP_aktien = {
         'LHA.DE': 6.379,
-        'H4ZJ.DE': 27.468,
+        'H4ZJ.DE': 27.446,
         # 'E127.MI': 47.942,
         'BTC-EUR': 37_000}
     return dict_aktien, dict_bezeichnung_aktien, dict_n_aktien, dict_EP_aktien
